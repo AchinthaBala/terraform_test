3d61fd3b72a3664a8619272441c7a4d7dd93c0ad
Achintha Balasooriya <36558525+AchinthaBala@users.noreply.github.com>
2026-05-26 22:11:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,17 @@
-resource "aws_vpc" "my_vpc" {
-  cidr_block           = var.mycidr
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+resource "aws_vpc" "my-vpc" {
+  cidr_block = var.vpc_cidr_block
 
   tags = {
-    Name        = "my-prod-vpc"
-    Environment = var.environment
-    Team        = "devops"
+    Name = var.vpc_name
   }
 }
 
-resource "aws_subnet" "my_private_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.pvt_subnet_cidr
-  availability_zone = var.pvt_subnet_az
-  tags = {
-    Name        = "my-prod-private-subnet"
-    Environment = var.environment
-    Team        = "devops"
-  }
-}
+resource "aws_subnet" "my-subnet" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = "us-east-1a"
 
-resource "aws_subnet" "my_public_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.pub_subnet_cidr
-  availability_zone = var.pubt_subnet_az
   tags = {
-    Name        = "my-prod-public-subnet"
-    Environment = var.environment
+    Name = var.subnet_name
   }
 }
